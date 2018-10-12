c9cb4bf843994583eba0178a8c9a513baf70db80
Matheus <matheus.ar11@hotmail.com>
2018-10-12 13:40:51 -0300

    I don't know what's happening

--- back_propagation__lm/bp.py
+++ back_propagation__lm/bp.py
@@ -1,6 +1,7 @@
 #bibliotecas numéricas
 from random import *
 from math import *
+import random
 
 #bibliotecas para plotagem
 import numpy as np
@@ -21,11 +22,10 @@ def sombreiro(x, y, option):
 			z.append(sin(sqrt(x[i]**2+y[i]**2))/sqrt(x[i]**2+y[i]**2))
 		return z
 
-
 #parametros essenciais
 training_goal = 0.0001
-learning_rate = 0.01
-number_of_neurons = 25
+learning_rate = 0.001
+number_of_neurons = 20
 number_of_points = 250
 
 #definicao das entradas para treinamento da rede
@@ -37,34 +37,34 @@ for i in range(0, number_of_points):
 x.sort()
 y.sort()
 X, Y = np.meshgrid(x, y)
-Z = sombreiro(X, Y, 'np_array')
 
+#saida para plot
+Z = sombreiro(X, Y, 'np_array')
 #saida para treinamento da rede
 z = sombreiro(x, y, '')
 
 #plot da superfície inicial
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(X, Y, Z)
+#ax.plot_surface(X, Y, Z)
 
 #definição da rede com BP
 neural_network = MLPRegressor(
-	hidden_layer_sizes = (number_of_neurons, number_of_neurons , number_of_neurons),
+	hidden_layer_sizes = (number_of_neurons, number_of_neurons),
 	solver = 'sgd',
-	activation = 'logistic',
-	learning_rate = 'constant',
+	activation = 'tanh',
+	learning_rate = 'adaptive',
 	learning_rate_init = learning_rate,
 	tol = training_goal
 )
 
 #formatando entrada para o scikit
 train_input = []
-aux = []
 for i in range(0, number_of_points):
+	aux = []
 	aux.append(x[i])
 	aux.append(y[i])
 	train_input.append(aux)
-	aux = []
 
 #treinamento da rede
 neural_network.fit(train_input, z)
@@ -75,19 +75,17 @@ x_predict = []
 y_predict = []
 
 for i in range(0, number_of_points):
-	x_predict.append(uniform(-10,10))
-	y_predict.append(uniform(-10,10))
-
-for i in range(0, number_of_points):
+	aux = []
+	x_predict.append(uniform(-10, 10))
+	y_predict.append(uniform(-10, 10))
 	aux.append(x_predict[i])
 	aux.append(y_predict[i])
 	predict_input.append(aux)
-	aux = []
 
 #predição do modelo
 predicted_output = neural_network.predict(predict_input)
 
-print(z, predicted_output)
+aux = np.column_stack((x_predict, y_predict, predicted_output))
 
 #plot dos pontos preditos
 ax.scatter(x_predict, y_predict, predicted_output, c='r', marker='o')
